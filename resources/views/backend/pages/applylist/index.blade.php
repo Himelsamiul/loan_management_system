cd2494f8dfa312cf73b95a5cfac80adbc9ae3fc7
Samiul Himel <alamhpl11@gmail.com>
2025-12-24 19:22:21 +0600

    note add on iindex apply

--- resources/views/backend/pages/applylist/index.blade.php
+++ resources/views/backend/pages/applylist/index.blade.php
@@ -4,6 +4,16 @@
 <div class="container-fluid py-4">
 
     <h2 class="mb-3 text-center text-primary fw-bold">All Loan Applications</h2>
+    <div class="alert alert-warning shadow-sm d-flex align-items-start gap-2 mb-4">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#856404" class="bi bi-exclamation-triangle-fill mt-1" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.964 0L.165 13.233c-.457.778.091 1.767.982 1.767h13.706c.89 0 1.438-.99.982-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1-2.002 0 1 1 0 0 1 2.002 0z"/>
+    </svg>
+    <div>
+        <strong>Important Note:</strong><br>
+        Please check the applicant’s full details and uploaded documents carefully before approving or rejecting any loan application.
+    </div>
+</div>
+
 
     {{-- ======================== --}}
     {{-- SEARCH + FILTER SECTION --}}
